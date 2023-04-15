9c1e8b6527ee2c0d0812c873d4ba8d88fd853af7
thidaoC0223g1 <daogiathi231290@gmail.com>
2023-04-15 16:44:36 +0700

    tao lop contact

---
+++ phonebook/src/model/Contact.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Contact {
+private String name;
+private String phonenumber;
+private Type type;
+
+    public Contact() {
+    }
+
+    public Contact(String name, String phonenumber, Type type) {
+        this.name = name;
+        this.phonenumber = phonenumber;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "name='" + name + '\'' +
+                ", phonenumber='" + phonenumber + '\'' +
+                ", type=" + type +
+                '}';
+    }
+}
