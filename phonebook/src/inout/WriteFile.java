b3c850690712421889525943bd101da36153bae7
thidaoC0223g1 <daogiathi231290@gmail.com>
2023-04-16 12:11:37 +0700

    tao ham write file

--- phonebook/src/inout/WriteFile.java
+++ phonebook/src/inout/WriteFile.java
@@ -1,4 +1,25 @@
 package inout;
 
+import model.Contact;
+
+import java.io.*;
+import java.util.List;
+
 public class WriteFile {
+    public void writeContact(List<Contact> contactList) {
+        File file = new File("./phonebook/txt/phonebook.dat");
+        try {
+            FileOutputStream fileoutput = new FileOutputStream(file);
+            try {
+                ObjectOutputStream listouput = new ObjectOutputStream(fileoutput);
+                listouput.writeObject(contactList);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
