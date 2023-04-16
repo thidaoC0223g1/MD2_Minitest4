ea4a59ffdef6583cc406ba1b7eefb92d3efe09a0
thidaoC0223g1 <daogiathi231290@gmail.com>
2023-04-16 12:11:13 +0700

    tao ham read file

--- phonebook/src/inout/ReadFile.java
+++ phonebook/src/inout/ReadFile.java
@@ -1,4 +1,27 @@
 package inout;
 
+
+import model.Contact;
+
+import java.io.*;
+import java.util.List;
+
 public class ReadFile {
+    public List<Contact> readContact() {
+        File file = new File("./phonebook/txt/phonebook.dat");
+        List<Contact> contactlist;
+        try {
+            FileInputStream fileinput = new FileInputStream(file);
+            ObjectInputStream listinput = new ObjectInputStream(fileinput);
+             contactlist = (List<Contact>) listinput.readObject();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+           return contactlist;
+    }
+
 }
